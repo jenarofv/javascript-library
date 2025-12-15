51369c5a5d40e4f19080b0969067143a5947390b
jenaro fv <jen4r0@gmail.com>
2025-12-15 17:47:33 -0600

    add Book constructor and addBookToLibrary

---
+++ script.js
@@ -0,0 +1,34 @@
+const myLibrary = [];
+
+function Book (title, author, pages, read) {
+  if (!new.target) {
+    throw Error("This function is a constructor, use the `new` keyword.");
+  }
+  if (typeof title === "string") {
+    this.title = title;
+  } else  {
+    throw Error("Book title should be string");
+  }
+  if (typeof author === "string") {
+    this.author = author;
+  } else  {
+    throw Error("Book author should be String");
+  }
+  if (typeof pages === "number") {
+    this.pages = pages;
+  } else  {
+    throw Error("Book pages should be Number");
+  }
+  if (typeof read === "boolean") {
+    this.read = pages;
+  } else  {
+    throw Error("Book read should be Number");
+  }
+
+  this.id = crypto.randomUUID();
+}
+
+function addBookToLibrary(title, author, pages, read) {
+  book = new Book(title, author, pages, read);
+  myLibrary.push(book);
+}

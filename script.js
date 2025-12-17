1441d59eb7a4b6587a9326c64eb1756790fb0bd2
jenaro fv <jen4r0@gmail.com>
2025-12-16 23:38:20 -0600

    add function to display books

--- script.js
+++ script.js
@@ -1,39 +1,39 @@
 const myLibrary = [];
 const form = document.querySelector("#book-form");
-booklist = document.querySelector("#booklist");
-
-const bookForm  = {
-  title : document.querySelector("#title"),
-  pages : document.querySelector("#pages"),
-  author : document.querySelector("#author"),
-  read : document.querySelector("#read"),
-}
-
+const booklist = document.querySelector("#booklist");
 const addBtn = document.querySelector("#add-book");
+const readCheckBox = document.querySelector("#read");
+const markAsRead = readCheckBox.nextElementSibling;
+const bookFormContent = {
+  author: document.querySelector("#author"),
+  title: document.querySelector("#title"),
+  pages: document.querySelector("#pages"),
+  read: document.querySelector("#read")
+};
 
 function Book (title, author, pages, read) {
   if (!new.target) {
-    throw Error("This function is a constructor, use the `new` keyword.");
+    alert("This function is a constructor, use the `new` keyword.");
   }
-  if (typeof title === "string") {
+  if (typeof title === "string" && title !== "") {
     this.title = title;
   } else  {
-    throw Error("Book title should be string");
+    alert("Book title should be non empty string");
   }
-  if (typeof author === "string") {
+  if (typeof author === "string" && author !== "") {
     this.author = author;
   } else  {
-    throw Error("Book author should be String");
+    alert("Book author should be non empty string");
   }
-  if (typeof pages === "number") {
+  if (typeof pages === "number" && pages > 0) {
     this.pages = pages;
   } else  {
-    throw Error("Book pages should be Number");
+    alert("Book pages should be at least 1");
   }
   if (typeof read === "boolean") {
     this.read = read;
   } else  {
-    throw Error("Book read should be boolean");
+    alert("Book read should be Boolean");
   }
 
   this.id = crypto.randomUUID();
@@ -41,48 +41,50 @@ function Book (title, author, pages, read) {
 
 function addBookToLibrary(title, author, pages, read) {
   book = new Book(title, author, pages, read);
+  if (title === "" || author === "" || pages < 0) {
+    return;
+  }
   myLibrary.push(book);
+  displayBook(book);
 }
 
-addBookToLibrary("bible", "god", 420, false);
-addBookToLibrary("The Call Of Cthulhu", "HP Lovecraft", 89, true);
-
-function displayBooks () {
-  myLibrary.forEach(book => {
-    row = document.createElement("tr");
-    author = document.createElement("td");
-    title = document.createElement("td");
-    pages = document.createElement("td");
-    read = document.createElement("td");
-    author.innerText = book.author;
-    title.innerText = book.title;
-    pages.innerText = book.pages;
-    read.innerText = book.read ? "✓" : "✗";
-    row.appendChild(author);
-    row.appendChild(title);
-    row.appendChild(pages);
-    row.appendChild(read);
-    booklist.appendChild(row);
-  });
+function displayBook(book) {
+  row = document.createElement("tr");
+  author = document.createElement("td");
+  title = document.createElement("td");
+  pages = document.createElement("td");
+  read = document.createElement("td");
+  row.id = book.id;
+  author.innerText = book.author;
+  title.innerText = book.title;
+  pages.innerText = book.pages;
+  read.innerText = book.read ? "✓" : "✗";
+  row.appendChild(author);
+  row.appendChild(title);
+  row.appendChild(pages);
+  row.appendChild(read);
+  booklist.appendChild(row);
 }
 
-displayBooks();
-
-function getBookFromForm () {
+function getBookFromForm() {
   addBookToLibrary(
-    bookForm.title.value,
-    bookForm.author.value,
-    bookForm.pages.valueAsNumber,
-    bookForm.read.checked
+    bookFormContent.title.value,
+    bookFormContent.author.value,
+    bookFormContent.pages.valueAsNumber,
+    bookFormContent.read.checked
   );
 }
 
-function clearForm () {
-}
-
-addBtn.addEventListener("click", e=> {
-  // e.preventDefault();
+addBtn.addEventListener("click", e => {
+  e.preventDefault();
   getBookFromForm();
-  console.table(myLibrary);
   form.reset();
-})
+});
+
+readCheckBox.addEventListener("click", e => {
+  if (readCheckBox.checked) {
+    markAsRead.classList.remove("hidden");
+  } else {
+    markAsRead.classList.add("hidden");
+  }
+});
